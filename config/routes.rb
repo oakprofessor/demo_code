c3e1f80bcc35a29b3a9b4f7069a20e72aceb1214
Phuc dog <onthidaihocabd@gmail.com>
2018-08-02 09:18:26 +0700

    1

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "static_pages#home"
+  get  "/room", to: "static_pages#room"
+  get  "/news", to: "static_pages#news"
+  get  "/contact", to: "static_pages#contact"
+  get  "/help", to: "static_pages#help"
+  get  "/signup",  to: "users#new"
+  post "/signup",  to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :users
+end
